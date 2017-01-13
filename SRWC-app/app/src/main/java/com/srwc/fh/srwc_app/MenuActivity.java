4ec372929c5653780d9efb83a24dadb2c61c3613
Pol Fernandez <pfclot@gmail.com>
2017-01-13 11:43:27 +0100

    MenuActivity

---
+++ SRWC-app/app/src/main/java/com/srwc/fh/srwc_app/MenuActivity.java
@@ -0,0 +1,10 @@
+package com.srwc.fh.srwc_app;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Pol on 13/01/2017.
+ */
+
+public class MenuActivity extends AppCompatActivity {
+}
